f3ebc3b6349684dc07070d5f50441eae706553a6
EC2 Default User <ec2-user@ip-10-0-7-31.ap-northeast-1.compute.internal>
2022-11-22 10:13:59 +0000

    front-back connect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
 
-function App() {
+const App = () => {
+  const url = '/users';
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {}, []);
+
   return (
-    <div className='App'>
-      <h1>Hello world</h1>
+    <div classname='App'>
+      <h1>Users</h1>
+      {users.map((user, index) => (
+        <div key={index}>{user.name}</div>
+      ))}
+      <div>
+        <Button
+          variant='contained'
+          onClick={() => {
+            fetch(url, { method: 'GET' })
+              .then((res) => res.json())
+              .then((data) => {
+                setUsers(data);
+              })
+              .catch((err) => {
+                console.log(err);
+                console.log('err');
+              });
+          }}
+        >
+          cssなくてもいい感じのボタン
+        </Button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
