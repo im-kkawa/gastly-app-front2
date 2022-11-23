e5765ce9e83edc523e661c06b9e169d55f30f3c0
EC2 Default User <ec2-user@ip-10-0-13-66.ap-northeast-1.compute.internal>
2022-11-23 05:48:06 +0000

    検索エリアの見た目を修正

--- src/components/searchShop/searchCondition.js
+++ src/components/searchShop/searchCondition.js
@@ -7,13 +7,14 @@ import Select from '@mui/material/Select';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
 import SearchIcon from '@mui/icons-material/Search';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 export default function SearchCondition(props) {
   const url = '/shops/test';
-
   const theme = createTheme({
     palette: {
       primary: {
@@ -25,7 +26,6 @@ export default function SearchCondition(props) {
       },
     },
   });
-
   const [area, setArea] = useState('');
 
   const onChangeSetArea = (event) => {
@@ -47,64 +47,62 @@ export default function SearchCondition(props) {
   };
 
   return (
-    <div>
-      <ThemeProvider theme={theme}>
-        <FormControl variant='standard'>
-          <Stack
-            direction='row'
-            spacing={4}
-            justifyContent='flex-start'
-            alignItems='flex-end'
+    <ThemeProvider theme={theme}>
+      <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+        <Stack
+          direction='row'
+          spacing={4}
+          justifyContent='flex-start'
+          alignItems='flex-end'
+        >
+          <InputLabel id='demo-simple-select-standard-label'>エリア</InputLabel>
+          <Select
+            labelId='demo-simple-select-standard-label'
+            id='demo-simple-select-standard'
+            value={area}
+            onChange={onChangeSetArea}
+            label='Area'
+            sx={{ minWidth: 180 }}
           >
-            <InputLabel id='demo-simple-select-standard-label'>
-              エリア
-            </InputLabel>
-            <Select
-              labelId='demo-simple-select-standard-label'
-              id='demo-simple-select-standard'
-              value={area}
-              onChange={onChangeSetArea}
-              label='Area'
+            <MenuItem value={1}>大手町</MenuItem>
+            <MenuItem value={2}>みなとみらい</MenuItem>
+            <MenuItem value={3}>木場</MenuItem>
+          </Select>
+          <Box
+            component='form'
+            sx={{
+              '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete='off'
+          >
+            <TextField
+              id='standard-basic'
+              label='人数'
+              variant='standard'
               sx={{ minWidth: 180 }}
-            >
-              <MenuItem value={1}>大手町</MenuItem>
-              <MenuItem value={2}>みなとみらい</MenuItem>
-              <MenuItem value={3}>木場</MenuItem>
-            </Select>
-            <Box
-              component='form'
-              sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
-                mb: '1.5rem',
-              }}
-              noValidate
-              autoComplete='off'
-            >
-              <TextField
-                id='standard-basic'
-                label='人数'
-                variant='standard'
-                sx={{ minWidth: 180 }}
-              />
-            </Box>
-            <Button
-              variant='outlined'
-              size='small'
-              startIcon={<SearchIcon onClick={onClickSearchShopIsSubmitted} />}
-              sx={{ height: '40px', mt: '6rem' }}
-            >
-              検索
-            </Button>
-            {/* <Button
-              variant='contained'
-              startIcon={<SearchIcon />}
-              onClick={onClickSearchShopIsSubmitted}
-            >
-              検索
-            </Button> */}
-          </Stack>
-        </FormControl>
-      </ThemeProvider>
-    </div>
+            />
+          </Box>
+          <Button
+            variant='outlined'
+            size='small'
+            startIcon={<SearchIcon />}
+            onClick={onClickSearchShopIsSubmitted}
+            sx={{ height: '40px', mt: '6rem' }}
+          >
+            検索
+          </Button>
+          {/* <Button
+            variant='contained'
+            size='small'
+            startIcon={<SearchIcon />}
+            onClick={onClickSearchShopIsSubmitted}
+            sx={{ height: '40px', mt: '6rem' }}
+          >
+            検索
+          </Button> */}
+        </Stack>
+      </FormControl>
+    </ThemeProvider>
   );
 }
